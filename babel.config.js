440f445498b4d29097929f82e1b3c3c7880339e7
Ihor <Ihor.Tymofieiev@gmail.com>
2019-10-12 15:45:24 +0300

    feat: create the base project structure

--- babel.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  presets: ['env', '@babel/preset-env', '@babel/preset-react'],
-  plugins: [
-    '@babel/plugin-proposal-class-properties',
-    '@babel/plugin-proposal-object-rest-spread',
-    'add-module-exports'
-  ]
-};
